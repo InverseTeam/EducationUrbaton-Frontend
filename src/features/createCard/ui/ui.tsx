200b7c6d45439341c6ec246ea8c790015fddd1e9
Dmitriy <dimastepanov.1995@mail.ru>
2023-11-21 21:55:54 +0500

    changed: groupCreadeCard => createCard, createCard

--- src/features/createCard/ui/ui.tsx
+++ src/features/createCard/ui/ui.tsx
@@ -1,13 +1,13 @@
 import styles from './ui.module.scss';
 import Plus from '../../../../public/globalIcons/plus.svg';
 import Image from 'next/image';
-export const GroupCreateCard = ({ onClick }: { onClick?: () => void }) => {
+export const GroupCreateCard = ({ onClick, title }: { onClick?: () => void; title: string }) => {
     return (
         <>
             <article onClick={onClick} className={styles.card}>
                 <span className={styles.create}>
                     <Image src={Plus} width={24} height={24} alt="Plus" />
-                    <h3 className={styles.blue}>Добавить группу</h3>
+                    <h3 className={styles.blue}>{title}</h3>
                 </span>
             </article>
         </>
